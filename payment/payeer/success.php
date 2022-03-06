9b99937e0db98e253728034a5cfce484d0bcbcb3
Maqsutjon2223 <100192830+Maqsutjon2223@users.noreply.github.com>
2022-03-06 10:30:05 +0500

    Delete success.php

--- payment/payeer/success.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['m_orderid']))
-{
-	$order_id = preg_replace('/[^a-zA-Z0-9_-]/', '', substr($_GET['m_orderid'], 0, 32));
-	header("Location: " . $SysValue['dir']['dir'] . "/users/order.html?orderId=" . $order_id . "#PphpshopOrder");
-}
-?>
